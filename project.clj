fbcf54acd0e7788620aa9b2939b77faf0217f018
Adrian Punga <adrian.punga@everymatrix.com>
2017-06-01 15:21:04 +0300

    Corrected license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.clojars.sqreept/consul-config-loader "1.0.0"
   :description "Loads all consul key value items starting at a given prefix."
   :url "https://github.com/adi/consul-config-loader"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [
                  [org.clojure/clojure "1.8.0"]
                  [consul-clojure "0.7.1"]
